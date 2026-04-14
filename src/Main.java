4fb3a6603c2de24a153beb303e2e41e5ecbcbb00
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-04-14 15:58:59 -0700

    Completed from emailLookup to hasCommonEfficient

--- src/Main.java
+++ src/Main.java
@@ -158,6 +158,7 @@ public class Main {
   // Otherwise, it returns "Person not found"
   // assume that each String is bounded by a constant length
   // What is the time complexity of this method?
+  // The time complexity is: O(n) where n = length of names
   // YOUR ANSWER HERE
   public static String emailLookup(String[] names, String[] emails, String queryName) {
     for(int i = 0; i < names.length; i++) {
@@ -174,14 +175,19 @@ public class Main {
   // Write this method to efficiently return the corresponding email or "Person not found" if appropriate
   // assume that each String is bounded by a constant length
   // What is the time complexity of your solution?
+  // The time complexity is: O(1)
   // YOUR ANSWER HERE
   public static String emailLookupEfficient(HashMap<String, String> namesToEmails, String queryName) {
-    return null;
+    if (namesToEmails.containsKey(queryName)) {
+      return namesToEmails.get(queryName);
+    }
+    return "Person not found";
   }
 
-  // What is the time complexity of this method?
   // assume that each String is bounded by a constant length
   // (assume the set and list have the same number of elements)
+  // What is the time complexity of this method?
+  // The time complexity is: O(n^2) where n = length of wordSet
   // YOUR ANSWER HERE
   public static boolean hasCommon(HashSet<String> wordSet, ArrayList<String> wordList) {
     for(String word : wordSet) {
@@ -191,12 +197,19 @@ public class Main {
     }
     return false;
   }
+
   // Rewrite hasCommon so it does the same thing as hasCommon, but with a better time complexity.
   // Do not change the datatype of wordSet or wordList.
   // assume that each String is bounded by a constant length
   // What is the time complexity of your new solution?
+  // The time complexity is: O(n) where n = length of wordlist
   // YOUR ANSWER HERE
   public static boolean hasCommonEfficient(HashSet<String> wordSet, ArrayList<String> wordList) {
+    for (String word : wordList) {
+      if (wordSet.contains(word)) {
+          return true;
+      }
+    }
     return false;
   }
 
